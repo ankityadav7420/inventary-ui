9bf1c22fb09cb5d2c7ffefe0a8be600889b687d7
ayadav <kumar2001y@gmail.com>
2026-06-02 01:30:19 +0530

    update

--- src/api/dashboardApi.js
+++ src/api/dashboardApi.js
@@ -1,11 +1,8 @@
 import api from '../services/axiosInstance';
 
-const BASE_URL = api.baseURL;
-
 export const getDashboardData = async () => {
   const token = localStorage.getItem('token');
-
-  const response = await api.get(`${BASE_URL}/products/dashboard`, {
+  const response = await api.get('/products/dashboard', {
     headers: {
       accept: 'application/json',
       Authorization: `Bearer ${token}`
